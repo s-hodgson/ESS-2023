9c2017f1ee9b7b4124af8ea3308f6945d712d9a7
Sam Hodgson <60526476+s-hodgson@users.noreply.github.com>
2023-02-24 14:42:15 +0000

    Initial commit

---
+++ ESS/Core/Src/main.c
@@ -0,0 +1,15 @@
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "ess_helper.h"
+
+int main(void)
+{
+	/* Initialize system */
+	HAL_Init();
+	/* Initialize peripherals on board */
+	ess_helper_init();
+	// Set all the LEDs to on.
+	*(uint32_t*)0x40020C14 = 0xF000;
+    while(1){
+    }
+}
